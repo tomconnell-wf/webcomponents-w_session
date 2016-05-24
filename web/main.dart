62f505185fa0d82612c7e55eaa3a534795fa0093
Thomas Connell <tom.connell@workiva.com>
2016-05-24 14:55:19 -0600

    Point to local w_session and w_transport

--- web/main.dart
+++ web/main.dart
@@ -6,7 +6,7 @@ import 'package:w_session/w_session.dart';
 
 main() async {
   print('before session-start');
-  Session session = new Session(sessionHost: Uri.parse('https://wk-dev.wdesk.org'));
+  Session session = new Session(sessionHost: Uri.parse('http://localhost:8001'));
   await session.start();
   print('after session-start');
 }
